b54c25388f65b71b7b8c42e3d79f94d2faaedab7
Gho2t-dev <fabianharrab1@gmail.com>
2026-07-12 02:06:20 +0200

    Add HTML and JavaScript files for Switch statement practice

---
+++ practice_grounds/Webdev Basics/JavaScript Practice/Switch Statements/app.js
@@ -0,0 +1,27 @@
+let dayName;
+
+switch (dayNumber) {
+    case 1:
+      dayName = "Monday";
+      break;
+    case 2:
+      dayName = "Tuesday";
+      break;
+    case 3:
+      dayName = "Wednesday";
+      break;
+    case 4:
+      dayName = "Thursday";
+      break;
+    case 5:
+      dayName = "Friday";
+      break;
+    case 6:
+      dayName = "Saturday";
+      break;
+    case 7:
+      dayName = "Sunday";
+      break;
+    default:
+      dayName = "Invalid day number. Please enter a number between 1 and 7.";
+  }
