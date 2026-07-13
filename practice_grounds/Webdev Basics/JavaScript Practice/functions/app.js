f1a766fc54d5020ca4f4c26405d107377645cab3
Gho2t-dev <fabianharrab1@gmail.com>
2026-07-13 16:02:23 +0200

    Add JavaScript functions example with happyBirthday and add functions, along with HTML file to run the script

---
+++ practice_grounds/Webdev Basics/JavaScript Practice/functions/app.js
@@ -0,0 +1,24 @@
+// This exercise file will explain functions in JavaScript
+
+// function functionname(argument1,argument2){
+//     function code
+// }
+
+// happy birthday function
+function happyBirthday(username, age){
+    console.log("Happy Birthday to you!");
+    console.log("Happy Birthday to you!");
+    console.log(`Happy Birthday dear ${username}!`);
+    console.log("Happy Birthday to you!");
+    console.log(`You are ${age} years old`);
+}
+
+// Calling the function:
+happyBirthday("Fabian", 22);
+
+// using return
+function add(x, y){
+    return x + y;
+}
+
+console.log(add(4, 3));
