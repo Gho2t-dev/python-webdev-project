0a04cb41c97de01f3090dd9b41e31c3136b0d1e0
Gho2t-dev <fabianharrab1@gmail.com>
2026-07-13 20:02:40 +0200

    Improve API error handling and update welcome message display first api call now working

--- practice_grounds/FastAPI test/app.js
+++ practice_grounds/FastAPI test/app.js
@@ -1,10 +1,18 @@
 // Test api calling with quotes API
 
+const welcomeMessage = document.getElementById("welcomeMessage");
 const connectBtn = document.getElementById("connectBtn");
 
 connectBtn.onclick = function(){
     fetch("http://127.0.0.1:8000/")
-    .then(response => response.json())
-    .then(data => console.log(data))
+    .then(response => {
+
+        if(!response.ok){
+            throw new error("The resource you tried to access does not exist");
+        }
+        return response.json();
+
+    })
+    .then(data => welcomeMessage.textContent = data.Message)
     .catch(error => console.error(error));
 }
