d52b29b2b43d7cb4f788516a77eb6a379c27d664
Gho2t-dev <fabianharrab1@gmail.com>
2026-07-14 23:20:35 +0200

    Add functionality to submit Quote ID and display corresponding quote WIP

--- practice_grounds/FastAPI test/app.js
+++ practice_grounds/FastAPI test/app.js
@@ -2,7 +2,9 @@
 
 const welcomeMessage = document.getElementById("welcomeMessage");
 const connectBtn = document.getElementById("connectBtn");
-const showAll = document.getElementById("showAll");
+const submitQuoteIdBtn = document.getElementById("submitQuoteIdBtn");
+const quoteId = document.getElementById("quoteId");
+const quoteOutput = document.getElementById("quoteOutput");
 
 connectBtn.onclick = function(){
     fetch("http://127.0.0.1:8000/")
@@ -18,16 +20,16 @@ connectBtn.onclick = function(){
     .catch(error => console.error(error));
 }
 
-showAll.onclick = function(){
-    fetch("http://127.0.0.1:8000/quotes", {method: "GET"})
+submitQuoteIdBtn.onclick = function(){
+     fetch(`http://127.0.0.1:8000/quotes/${quoteId}`)
     .then(response => {
 
-        if(!response.ok){ // check if the response is NOT ok
+        if(!response.ok){
             throw new Error("The resource you tried to access does not exist");
         }
-        return response.json(); //turn the response into a js object
+        return response.json();
 
     })
-    .then(data => console.log(data))
+    .then(data => quoteOutput.textContent = data)
     .catch(error => console.error(error));
 }
