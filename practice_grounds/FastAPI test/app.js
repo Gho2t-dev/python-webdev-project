02b7a2c7119305db4dde715e31b8971f48cdb92d
Gho2t-dev <fabianharrab1@gmail.com>
2026-07-14 14:19:18 +0200

    Add functionality to fetch and display quotes from API

--- practice_grounds/FastAPI test/app.js
+++ practice_grounds/FastAPI test/app.js
@@ -2,17 +2,32 @@
 
 const welcomeMessage = document.getElementById("welcomeMessage");
 const connectBtn = document.getElementById("connectBtn");
+const showAll = document.getElementById("showAll");
 
 connectBtn.onclick = function(){
     fetch("http://127.0.0.1:8000/")
     .then(response => {
 
         if(!response.ok){
-            throw new error("The resource you tried to access does not exist");
+            throw new Error("The resource you tried to access does not exist");
         }
         return response.json();
 
     })
     .then(data => welcomeMessage.textContent = data.Message)
+    .catch(error => console.error(error));
+}
+
+showAll.onclick = function(){
+    fetch("http://127.0.0.1:8000/quotes", {method: "GET"})
+    .then(response => {
+
+        if(!response.ok){ // check if the response is NOT ok
+            throw new Error("The resource you tried to access does not exist");
+        }
+        return response.json(); //turn the response into a js object
+
+    })
+    .then(data => console.log(data))
     .catch(error => console.error(error));
 }
