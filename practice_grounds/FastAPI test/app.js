0f3614c04eaa907722493d98e72ea6db86b36ba4
Fabian. H <fabian.harrab1@gmail.com>
2026-07-15 21:42:48 +0200

    commit

--- practice_grounds/FastAPI test/app.js
+++ practice_grounds/FastAPI test/app.js
@@ -24,6 +24,7 @@ connectBtn.onclick = function(){
 }
 
 submitQuoteIdBtn.onclick = function(){
+    console.log(typeof quoteId)
     fetch(`http://127.0.0.1:8000/quotes/${quoteId.value}`)
     .then(response => {
 
